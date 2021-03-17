54279548d4acdc38f829e08a75ae5b4e0a060f00
Przemysław Tasarz <przemek433@interia.eu>
2021-03-17 17:23:25 +0100

    sidenav

--- script.js
+++ script.js
@@ -5,26 +5,38 @@ const DATA = [
               { id: 'd4', value: 0.135, date: '2019' },
               { id: 'd5', value: 0.1, date: '2020' },
             ];
-let sql_data = [];
-d3.json("getdata.php").then(function(data){
-	let array = data[0];
-	console.log(array);
-	for(let i = 0; i < Object.keys(array).length; i++) {
-		if(!isNaN(parseInt(Object.keys(array)[i]))) {
-			let push_object_data = {id: "d"+(i+1), date: Object.keys(array)[i], value: array[Object.keys(array)[i]]};
-			sql_data.push(push_object_data);
-		}
-	}
-});
-console.log(sql_data);
-const container = document.getElementById("dywidenda");
-let chart = new Chart(container, DATA, "Dywidenda");
-const sql_container = document.getElementById("rok");
-let chart2 = new Chart(sql_container, sql_data, "Dane z SQLa");
-function draw()
-{
-  chart.refresh();
-  chart2.refresh();
+var sql_data = [];
+var charts = [];
+function load(){
+  d3.json("getdata.php").then(function(data){
+  	let array = data[0];
+  	console.log(array);
+  	for(let i = 0; i < Object.keys(array).length; i++) {
+  		if(!isNaN(parseInt(Object.keys(array)[i]))) {
+  			let push_object_data = {id: "d"+(i+1), date: Object.keys(array)[i], value: array[Object.keys(array)[i]]};
+  			sql_data.push(push_object_data);
+  		}
+  	}
+  });
+  const container = document.getElementById("dywidenda");
+  let chart = new Chart(container, DATA, "Dywidenda");
+
+  const sql_container = document.getElementById("rok");
+  let chart2 = new Chart(sql_container, sql_data, "Dane z SQLa");
+  charts.push(chart);
+  charts.push(chart2);
+  draw();
 }
-window.onload = draw;
+function draw() {
+  for(let i = 0; i < charts.length; i++) {
+    charts[i].refresh();
+  }
+}
+function openNav() {
+  document.getElementById("sidenav").style.width = "25%";
+}
+function closeNav() {
+  document.getElementById("sidenav").style.width = "0px";
+}
+window.onload = load;
 window.onresize = draw;
